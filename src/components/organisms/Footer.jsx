5866f3a09383922c58160256ecd692c45c8fd02d
degleg <legoff.d@outlook.com>
2025-04-24 20:01:04 +0200

    Create Header and Footer components

---
+++ src/components/organisms/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer>
+            <p>This is the footer for now.</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/organisms/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header>
+            <p>This is the header for now</p>
+        </header>
+    );
+}
+
+export default Header;
