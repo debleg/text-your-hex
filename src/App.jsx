c2d76c9f1c5e14119f7c31ac291a278691c787f0
degleg <legoff.d@outlook.com>
2025-04-24 19:52:39 +0200

    Set up Router with Home and About page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Home from "./components/pages/Home"
+import About from "./components/pages/About"
 
 
 function App() {
-
   return (
-    <>
-      
-    </>
+<Router>
+  <Routes>
+    <Route path="/" element={<Home/>} />
+    <Route path="/about" element={<About/>} />
+    <Route path="*" element={<Home/>}/>
+  </Routes>
+</Router>
   )
 }
 
